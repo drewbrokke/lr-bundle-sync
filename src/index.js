1491e21cf84b30e99284a7b26177fcb929746112
drewbrokke <drew.brokke@liferay.com>
2015-01-30 16:57:13 -0800

    Use gulp-watcher instead (faster startup and built-in output)

--- src/index.js
+++ src/index.js
@@ -2,35 +2,37 @@ var gulp = require('gulp');
 var path = require('path');
 var chalk = require('chalk');
 var fs = require('fs-extra');
+var watch = require('gulp-watch');
 require('date-format-lite');
 
+// Variables
+
 var config = fs.readJsonSync('conf/config.json');
+
 var liferayHome = config.liferayHome;
 var bundleHome = config.bundleHome;
 
 var docrootHome = path.join(liferayHome, 'portal-web', 'docroot');
 var rootHome = path.join(bundleHome, 'tomcat-7.0.42', 'webapps', 'ROOT');
 
-function startGulpWatch(src, dest) {
-	console.log(chalk.bgBlack.gray('Starting watcher...'));
+var watcherOptions = {
+	name: 'Liferay Source Watcher',
+	verbose: true
+};
 
-	gulp.watch(path.join(src,'**/*.*'), function(event) {
-		var filePath = path.dirname(event.path).replace(src, dest);
-
-		gulp.src(event.path)
-			.pipe(gulp.dest(filePath));
-
-		var now = new Date();
-		var time = now.format("hh:mm:ss")
-
-		console.log(chalk.yellow(time) + ' - ' +  chalk.green('Updated file: ') + event.path.replace(src, ''));
-	});
-
-	console.log(chalk.bgBlack.gray('Listening for changes to') + chalk.bgBlack(' \"docroot\".') + '\n');
-}
+// Methods
 
 function printError(error) {
 	console.log(chalk.red(error));
+}
+
+function startGulpWatch(src, dest) {
+	console.log(chalk.bgBlack.gray('Starting watcher...'));
+
+	watch(path.join(src,'**/*.*'), watcherOptions)
+		.pipe(gulp.dest(dest));
+
+	console.log(chalk.bgBlack.gray('Listening for changes to') + chalk.bgBlack(' \"docroot\".') + '\n');
 }
 
 if (!liferayHome.length || !bundleHome.length) {
